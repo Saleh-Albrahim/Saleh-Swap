b2460ab97cd65261259ca93b347f4f6ffa07460c
Saleh Alibrahim <s.albrahim@outlook.com>
2021-11-06 02:53:33 +0300

    feat: 🎸 Completing the ERC-20 Token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,17 @@
+const Token = artifacts.require('Token');
 const EthSwap = artifacts.require('EthSwap');
 
-module.exports = function (deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async (deployer) => {
+  // deploy token
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  // deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  const totalSupply = await token.totalSupply();
+
+  // Transfer all the tokens to the EthSwap address
+  await token.transfer(ethSwap.address, totalSupply.toString());
 };
