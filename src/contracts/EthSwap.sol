f7641286055898b8d213e72d8b4fb9b602a126a5
Saleh Alibrahim <s.albrahim@outlook.com>
2021-11-06 08:05:24 +0300

    feat: 🎸 Complete the Sell and Buy smart contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -7,10 +7,17 @@ contract EthSwap {
     Token public token;
     uint public rate=100;
 
-    event TokenPurchased(
+    event TokensPurchased(
         address account,
         address token,
-        uint amout,
+        uint amount,
+        uint rate
+    );
+
+     event TokensSold(
+        address account,
+        address token,
+        uint amount,
         uint rate
     );
 
@@ -22,9 +29,34 @@ contract EthSwap {
         // Redemption rate = # of tokens they receive for 1 ether
         // About of Ethereum * Redemption rate 
         uint tokenAmount = msg.value * rate;
+
+        // Require that EthSwap has enough tokens
+        require(token.balanceOf(address(this))>= tokenAmount);
+
         token.transfer(msg.sender, tokenAmount);
 
         // Emit an event 
-        emit TokenPurchased(msg.sender, address(token),tokenAmount,rate);
+        emit TokensPurchased(msg.sender, address(token),tokenAmount,rate);
+    }
+
+    function sellTokens(uint _amount) public {
+
+        // User can't sell tokens more than what they have
+        require(token.balanceOf(msg.sender)>= _amount);
+
+        // Calculate the amount of Ether to redeem  
+        uint ethAmount = _amount / rate;
+
+        // Require that EthSwap has enough Ether
+        require(address(this).balance>=ethAmount);
+
+        // Transfer Saleh Token from the seller address to the swap address
+        token.transferFrom(msg.sender,address(this), _amount);
+
+        // Transfer ethereum token to the seller address
+        msg.sender.transfer(ethAmount);
+
+        // Emit an event 
+        emit TokensPurchased(msg.sender, address(token),_amount,rate);
     }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -52,11 +52,10 @@ contract('EthSwap', ([deployer, investor]) => {
     it('Allows user to instantly purchase tokens from ethSwap for a fixed price', async () => {
       // Check investor token balance after purchase
       const investorSalehBalance = await token.balanceOf(investor);
-      console.log('Saleh balance in the investor wallet : ' + fromWei(investorSalehBalance.toString()));
+      console.log(`Saleh balance in the investor wallet : ${fromWei(investorSalehBalance.toString())}`);
       assert.equal(investorSalehBalance.toString(), toWei('100'));
 
       // Check ethSwap balance after purchase
-
       const swapSalehBalance = await token.balanceOf(ethSwap.address);
       console.log(`Saleh balance in the swap wallet : ${fromWei(swapSalehBalance.toString())}`);
       assert.equal(swapSalehBalance.toString(), toWei('999900'));
@@ -66,45 +65,49 @@ contract('EthSwap', ([deployer, investor]) => {
       assert.equal(swapEthBalance.toString(), toWei('1'));
 
       // Check logs to ensure event was emitted with correct data
-      // const event = result.logs[0].args;
-      // assert.equal(event.account, investor);
-      // assert.equal(event.token, token.address);
-      // assert.equal(event.amount.toString(), tokens('100').toString());
-      // assert.equal(event.rate.toString(), '100');
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), toWei('100'));
+      assert.equal(event.rate.toString(), '100');
     });
   });
 
-  // describe('sellTokens()', async () => {
-  //   let result;
+  describe('sellTokens()', async () => {
+    let result;
 
-  //   before(async () => {
-  //     // Investor must approve tokens before the purchase
-  //     await token.approve(ethSwap.address, tokens('100'), { from: investor });
-  //     // Investor sells tokens
-  //     result = await ethSwap.sellTokens(tokens('100'), { from: investor });
-  //   });
+    before(async () => {
+      // Investor must approve tokens before the purchase
+      await token.approve(ethSwap.address, toWei('100'), { from: investor });
+      // Investor sells tokens
+      result = await ethSwap.sellTokens(toWei('100'), { from: investor });
+    });
 
-  //   it('Allows user to instantly sell tokens to ethSwap for a fixed price', async () => {
-  //     // Check investor token balance after purchase
-  //     let investorBalance = await token.balanceOf(investor);
-  //     assert.equal(investorBalance.toString(), tokens('0'));
+    it('Allows user to instantly sell tokens to ethSwap for a fixed price', async () => {
+      // Check investor token balance after purchase
+      const investorSalehBalance = await token.balanceOf(investor);
+      console.log(`Saleh balance in the investor wallet : ${fromWei(investorSalehBalance.toString())}`);
+      assert.equal(investorSalehBalance.toString(), toWei('0'));
 
-  //     // Check ethSwap balance after purchase
-  //     let ethSwapBalance;
-  //     ethSwapBalance = await token.balanceOf(ethSwap.address);
-  //     assert.equal(ethSwapBalance.toString(), tokens('1000000'));
-  //     ethSwapBalance = await web3.eth.getBalance(ethSwap.address);
-  //     assert.equal(ethSwapBalance.toString(), web3.utils.toWei('0', 'Ether'));
+      // Check ethSwap balance after purchase
 
-  //     // Check logs to ensure event was emitted with correct data
-  //     const event = result.logs[0].args;
-  //     assert.equal(event.account, investor);
-  //     assert.equal(event.token, token.address);
-  //     assert.equal(event.amount.toString(), tokens('100').toString());
-  //     assert.equal(event.rate.toString(), '100');
+      const swapSalehBalance = await token.balanceOf(ethSwap.address);
+      console.log(`Saleh balance in the swap wallet : ${fromWei(swapSalehBalance.toString())}`);
+      assert.equal(swapSalehBalance.toString(), toWei('1000000'));
 
-  //     // FAILURE: investor can't sell more tokens than they have
-  //     await ethSwap.sellTokens(tokens('500'), { from: investor }).should.be.rejected;
-  //   });
-  // });
+      const swapEthBalance = await web3.eth.getBalance(ethSwap.address);
+      console.log(`Ethereum balance in the swap wallet : ${fromWei(swapEthBalance.toString())}`);
+      assert.equal(swapEthBalance.toString(), toWei('0'));
+
+      // Check logs to ensure event was emitted with correct data
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), toWei('100'));
+      assert.equal(event.rate.toString(), '100');
+
+      // FAILURE: investor can't sell more tokens than they have
+      await ethSwap.sellTokens(toWei('500'), { from: investor }).should.be.rejected;
+    });
+  });
 });
