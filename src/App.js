2278c6e2447ff1f55047aab34cc72c4a000a0c9b
Saleh Alibrahim <s.albrahim@outlook.com>
2021-11-07 01:25:47 +0300

    feat: 🎸 Fetch the contract from the backend to the frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React,{Fragment, useEffect,useState} from 'react';
 import './App.css';
 import Web3 from 'web3';
+import EthSwap from './abis/EthSwap.json';
+import Token from './abis/Token.json';
 import Navbar from './components/ui/Navbar';
 
 const App = () => {
 
   const [account, setAccount] = useState('');
   const [balance, setBalance] = useState('');
+  const [tokenContract, setTokenContract] = useState({});
 
   // Connect to metaMask
   useEffect(() => {
@@ -26,12 +29,28 @@ const App = () => {
     const loadBlockChainData = async () => {
       const web3 = window.web3;
 
+      // Store the account in the account state
       const accounts = await web3.eth.getAccounts();
       await setAccount(accounts[0]);
 
+      // Store the balance in the balance state
       const ethBalance = await web3.eth.getBalance(accounts[0]);
       setBalance(ethBalance);
-      console.log(ethBalance);
+
+      // Get network ID
+      const networkId = await web3.eth.net.getId();
+      const tokenData = Token.networks[networkId];
+
+      // Check if the contract deployed to the detected network
+      if (tokenData) {
+        const token = new web3.eth.Contract(Token.abi, tokenData.address);
+        setTokenContract(token);
+        console.log(token);
+      } else {
+        alert('Contract not deployed to the detected network');
+      }
+    
+      
     }
     loadBlockChainData();
   }, []);
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -8,8 +8,8 @@ function Navbar({ account }) {
   return (
     <nav class='navbar navbar-dark  fixed-top bg-dark p-3 shadow'>
       <p className='navbar-brand  my-auto'>Ethereum Market Place</p>
-      <ul class='navbar-nav d-inline'>
-        <li class='navbar-text'>
+      <ul class='navbar-nav d-inline my-auto'>
+        <li>
           <span className='nav-address'>{account}</span>
           {identicon && (
             <img className='rounded mr-5' width='50' height='50' src={`data:image/png;base64,${identicon}`} alt='identicon'></img>
