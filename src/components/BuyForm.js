ca0a61c6c5f31b24f9d9a35fda10e4e0034e43fa
Saleh Alibrahim <s.albrahim@outlook.com>
2021-11-07 04:15:33 +0300

    refactor: 💡 Refactor the code and Fix the UI

--- src/components/BuyForm.js
+++ src/components/BuyForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ethLogo from '../img/eth.png';
+import salehLogo from '../img/saleh.png';
 
 function BuyForm({ accountEthBalance, accountTokenBalance, buyTokens }) {
   const [input, setInput] = useState(0);
@@ -16,10 +17,10 @@ function BuyForm({ accountEthBalance, accountTokenBalance, buyTokens }) {
       }}
     >
       <div>
-        <label className='float-left'>
-          <b>Input&nbsp;</b>
+        <label class='d-flex'>
+          <b class='me-auto font-weight-bold'>Input</b>
+          <div class='text-muted'>{window.web3.utils.fromWei(accountEthBalance, 'Ether')}</div>
         </label>
-        <span className='float-right text-muted'>Balance: {window.web3.utils.fromWei(accountEthBalance, 'Ether')}</span>
       </div>
       <div className='input-group mb-4'>
         <input
@@ -41,25 +42,25 @@ function BuyForm({ accountEthBalance, accountTokenBalance, buyTokens }) {
           </div>
         </div>
       </div>
-      <div>
-        <label className='float-left'>
-          <b>Output&nbsp;</b>
-        </label>
-        <span className='float-right text-muted'>Balance: {window.web3.utils.fromWei(accountTokenBalance, 'Ether')}</span>
-      </div>
+
+      <label class='d-flex'>
+        <b class='me-auto font-weight-bold'>Output</b>
+        <div class='text-muted'>{window.web3.utils.fromWei(accountTokenBalance, 'Ether')}</div>
+      </label>
       <div className='input-group mb-2'>
         <input type='text' className='form-control form-control-lg' placeholder='0' value={input * 100} disabled />
         <div className='input-group-append'>
           <div className='input-group-text p-2'>
-            <img src={ethLogo} height='32' alt='' />
+            <img className='rounded-circle' src={salehLogo} height='32' alt='' />
             &nbsp; SALEH
           </div>
         </div>
       </div>
-      <div className='mb-5'>
-        <span className='float-left text-muted'>Exchange Rate</span>
-        <span className='float-right text-muted'>1 ETH = 100 SALEH</span>
-      </div>
+
+      <label class='d-flex mb-5 mt-3'>
+        <b class='me-auto font-weight-bold'>Exchange Rate</b>
+        <div class='text-muted'>1 ETH = 100 SALEH</div>
+      </label>
       <button type='submit' className='btn btn-primary btn-lg btn-block w-100'>
         SWAP
       </button>
--- src/components/SellForm.js
+++ src/components/SellForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ethLogo from '../img/eth.png';
+import salehLogo from '../img/saleh.png';
 
 function SellForm({ accountEthBalance, accountTokenBalance, sellTokens }) {
   const [output, setOutput] = useState(0);
@@ -14,12 +15,11 @@ function SellForm({ accountEthBalance, accountTokenBalance, sellTokens }) {
         sellTokens(etherAmount);
       }}
     >
-      <div>
-        <label className='float-left'>
-          <b>Input&nbsp;</b>
-        </label>
-        <span className='float-right text-muted'>Balance: {window.web3.utils.fromWei(accountTokenBalance, 'Ether')}</span>
-      </div>
+      <label class='d-flex'>
+        <b class='me-auto font-weight-bold'>Input</b>
+        <div class='text-muted'>{window.web3.utils.fromWei(accountTokenBalance, 'Ether')}</div>
+      </label>
+
       <div className='input-group mb-4'>
         <input
           type='number'
@@ -35,17 +35,16 @@ function SellForm({ accountEthBalance, accountTokenBalance, sellTokens }) {
         />
         <div className='input-group-append'>
           <div className='input-group-text p-2'>
-            <img src={ethLogo} height='32' alt='' />
+            <img className='rounded-circle' src={salehLogo} height='32' alt='' />
             &nbsp; SALEH
           </div>
         </div>
       </div>
-      <div>
-        <label className='float-left'>
-          <b>Output&nbsp;</b>
-        </label>
-        <span className='float-right text-muted'>Balance: {window.web3.utils.fromWei(accountEthBalance, 'Ether')}</span>
-      </div>
+
+      <label class='d-flex'>
+        <b class='me-auto'>Output</b>
+        <div class='text-muted'>{window.web3.utils.fromWei(accountEthBalance, 'Ether')}</div>
+      </label>
       <div className='input-group mb-2'>
         <input type='text' className='form-control form-control-lg' placeholder='0' value={output / 100} disabled />
         <div className='input-group-append'>
@@ -55,9 +54,10 @@ function SellForm({ accountEthBalance, accountTokenBalance, sellTokens }) {
           </div>
         </div>
       </div>
-      <div className='mb-5'>
-        <span className='float-left text-muted'>Exchange Rate 100 SALEH = 1 ETH</span>
-      </div>
+      <label class='d-flex mb-5 mt-3'>
+        <b class='me-auto font-weight-bold'>Exchange Rate</b>
+        <div class='text-muted'>1 ETH = 100 SALEH</div>
+      </label>
       <button type='submit' className='btn btn-primary btn-lg btn-block w-100'>
         SWAP
       </button>
